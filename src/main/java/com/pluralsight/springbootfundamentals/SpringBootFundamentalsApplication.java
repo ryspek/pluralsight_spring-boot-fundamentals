557ff9a5f601fea326cbc5aae175ea6753bf9f76
Ryspek Baibosunov <ryspek.baybosunov@gmail.com>
2022-04-25 17:31:40 +0600

    initial commit

---
+++ src/main/java/com/pluralsight/springbootfundamentals/SpringBootFundamentalsApplication.java
@@ -0,0 +1,14 @@
+package com.pluralsight.springbootfundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFundamentalsApplication.class, args);
+		System.out.println("Hello Pluralsight!");
+	}
+
+}
